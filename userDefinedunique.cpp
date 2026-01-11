22871201e429a5d6db64e63e5206b30d7c834117
akashbhatkar <114064348+akashbhatkar@users.noreply.github.com>
2026-01-11 16:29:26 +0530

    user defined unique pointer

--- userDefinedunique.cpp
+++ userDefinedunique.cpp
@@ -1,64 +1,58 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-template<typename T>
-class unique
-{
-    private:
-    T* ptr = nullptr;
-    public:
-    explicit unique(T* p = nullptr)
-    {
-        ptr = p;
+template <typename T>
+class MyUniquePtr {
+private:
+    T* ptr = nullptr; // The actual raw pointer
+
+public:
+    // 1. Constructor
+    explicit MyUniquePtr(T* p = nullptr) : ptr(p) {}
+
+    // 2. Destructor (The "RAII" part)
+    ~MyUniquePtr() {
+        delete ptr;
+        std::cout << "Memory Deleted\n";
     }
 
-    unique(unique& uobj) = delete;
-    unique& operator =(unique& uboj) = delete;
+    // 3. Disable Copying (No two pointers can own the same memory)
+    MyUniquePtr(const MyUniquePtr&) = delete;
+    MyUniquePtr& operator=(const MyUniquePtr&) = delete;
 
-    unique(unique&& obj)
-    {
-        ptr = obj.ptr;
-        obj.ptr = nullptr;
+    // 4. Enable Moving (Transfer ownership)
+    MyUniquePtr(MyUniquePtr&& other) noexcept {
+        ptr = other.ptr;     // Take the pointer
+        other.ptr = nullptr; // Leave the old one empty
     }
-    unique& operator = (unique&& obj) noexcept
-    {
-        if(this != &obj)
-        {
-            delete ptr;
-            ptr = obj.ptr;
-            obj.ptr = nullptr;
+
+    MyUniquePtr& operator=(MyUniquePtr&& other) noexcept {
+        if (this != &other) {
+            delete ptr;          // Delete existing resource
+            ptr = other.ptr;     // Take the new one
+            other.ptr = nullptr; // Nullify source
         }
         return *this;
     }
 
-    T& operator *() const
-    {
-        return *ptr;
-    }
-    T* operator ->()const
-    {
-        return ptr;
-    }
-    ~unique()
-    {
-        delete ptr;
-        cout<<"memory deleted"<<endl;
-    }
-    T* get()
-    {
-        return ptr;
-    }
+    // 5. Overload dereference operators
+    T& operator*() const { return *ptr; }
+    T* operator->() const { return ptr; }
+
+    T* get() const { return ptr; }
 };
 
-int main()
-{
-    unique<int> p(new int(50));
-    cout<<*p<<endl;
-
-    unique<int> p2 = move(p);
-    cout<<*p2<<endl;
-    if(p.get()==nullptr)
+int main() {
     {
-        cout<<"memeory is transfered"<<endl;
-    }
+        MyUniquePtr<int> p1(new int(50));
+        std::cout << "Value: " << *p1 << std::endl;
+
+        // MyUniquePtr<int> p2 = p1; // This would cause a Compile Error (Good!)
+        
+        MyUniquePtr<int> p2 = std::move(p1); // Ownership transferred
+        if (p1.get() == nullptr) {
+            std::cout << "p1 is now empty.\n";
+        }
+    } // p2 goes out of scope here, memory is automatically deleted
+    
+    return 0;
 }
